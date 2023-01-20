21dd7ecd1ca2bfd110ca6ab058d388e35fe47b8d
Bryce W <58643544+02bwilson@users.noreply.github.com>
2023-01-20 07:48:37 -0500

    Added base audio sampler

--- audiosampler.py
+++ audiosampler.py
@@ -8,30 +8,36 @@ class AudioSampler:
     _VERSION_ = "1.0"
 
     def __init__(self, channels=2, rate=44100):
-        p = pyaudio.PyAudio()
+
+        self.stream = None
+
+        self.p = pyaudio.PyAudio()
 
         self.CHANNELS = channels
         self.RATE = rate
 
-        stream = p.open(format=pyaudio.paFloat32,
+    def start_stream(self):
+
+        self.stream = self.p.open(format=pyaudio.paFloat32,
                         channels=self.CHANNELS,
                         rate=self.wRATE,
                         output=True,
                         input=True,
-                        stream_callback=self.callback)
+                        stream_callback=callback)
 
-        stream.start_stream()
+        self.stream.start_stream()
 
-        while stream.is_active():
-            time.sleep(20)
-            stream.stop_stream()
+    def stop_stream(self):
+
+        if self.stream.is_active():
+            self.stream.stop_stream()
             print("Stream is stopped")
 
-        stream.close()
+        self.stream.close()
 
-        p.terminate()
+        self.p.terminate()
 
-    def callback(in_data, frame_count, time_info, flag):
-        # using Numpy to convert to array for processing
-        # audio_data = np.fromstring(in_data, dtype=np.float32)
-        return in_data, pyaudio.paContinue
+def callback(in_data, frame_count, time_info, flag):
+    # using Numpy to convert to array for processing
+    # audio_data = np.fromstring(in_data, dtype=np.float32)
+    return in_data, pyaudio.paContinue
