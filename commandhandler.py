ce84b447f6252fc607bc980008047cf2c23828d3
Bryce W <58643544+02bwilson@users.noreply.github.com>
2023-01-20 07:49:00 -0500

    Changed command handler (may end up removing this)

--- commandhandler.py
+++ commandhandler.py
@@ -11,10 +11,6 @@ class CommandHandler:
                          "exit": "Exit the program."}
         self.exit_flag = False
 
-        if args.rtlsdr:
-            self.cmd_list += {"setgain", "Set the gain of your RTLSDR",
-                              "set"}
-
         print("Command Handler v{} initilized!".format(self._VERSION_))
 
         self.cmdthrd = threading.Thread(target=self.await_cmd)
@@ -31,8 +27,25 @@ class CommandHandler:
                 break
 
     def handle_cmd(self, cmd):
-        if cmd.lower() not in self.cmd_list.keys():
+        cmd = cmd.lower().split()
+        if cmd[0] not in self.cmd_list.keys():
             print("Invalid Command!")
+            return
 
-        if cmd == "exit":
-            self.exit_flag = True
+        if cmd[0] == "exit":
+            self.exit_handler()
+        elif cmd[0] == "help":
+            self.help_handler()
+
+        elif cmd[0] == "setgain":
+            self.setgain_handler(cmd)
+
+    def help_handler(self):
+        for command in self.cmd_list.keys():
+            print("{} \t {}".format(command, self.cmd_list[command]))
+
+    def exit_handler(self):
+        self.exit_flag = True
+
+    def setgain_handler(self, cmd):
+        self.sdr.set_gain(cmd[1])
