acb26990d84d68fcc9edf6ed7da2b6b736fe338c
Bryce W <58643544+02bwilson@users.noreply.github.com>
2023-01-20 19:30:39 -0500

    Overhaul, added to thread, added data functions

--- audiosampler.py
+++ audiosampler.py
@@ -1,34 +1,36 @@
-import time
-
 import pyaudio
 import numpy as np
+from PyQt6.QtCore import QThread, pyqtSignal
 
 
-class AudioSampler:
+class AudioSampler(QThread):
     _VERSION_ = "1.0"
 
-    def __init__(self, channels=2, rate=44100):
+    update = pyqtSignal(np.ndarray)
+    def __init__(self, mw=None, channels=1, rate=44100):
+        super().__init__()
 
         self.stream = None
-
+        self.data = [0]
         self.p = pyaudio.PyAudio()
-
+        self.mw = mw
         self.CHANNELS = channels
         self.RATE = rate
+        self.audio_cache = []
 
     def start_stream(self):
-
         self.stream = self.p.open(format=pyaudio.paFloat32,
-                        channels=self.CHANNELS,
-                        rate=self.wRATE,
-                        output=True,
-                        input=True,
-                        stream_callback=callback)
+                                  channels=self.CHANNELS,
+                                  rate=self.RATE,
+                                  output=True,
+                                  input=True,
+                                  stream_callback=self.callback)
 
         self.stream.start_stream()
+        while self.stream.is_active():
+            pass
 
     def stop_stream(self):
-
         if self.stream.is_active():
             self.stream.stop_stream()
             print("Stream is stopped")
@@ -37,7 +39,34 @@ class AudioSampler:
 
         self.p.terminate()
 
-def callback(in_data, frame_count, time_info, flag):
-    # using Numpy to convert to array for processing
-    # audio_data = np.fromstring(in_data, dtype=np.float32)
-    return in_data, pyaudio.paContinue
+    def plot_data(self, data):
+        self.update.emit(data)
+
+    def get_audio_devices(self):
+        devices = []
+        info = self.p.get_host_api_info_by_index(0)
+        numdevices = info.get('deviceCount')
+        for i in range(0, numdevices):
+            if (self.p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+                devices += [str((str(i) + " - " + self.p.get_device_info_by_host_api_device_index(0, i).get('name')))]
+        return devices
+
+    def set_audio_device(self, id):
+        self.stream = self.p.open(format=pyaudio.paFloat32,
+                                  channels=self.CHANNELS,
+                                  rate=self.RATE,
+                                  output=True,
+                                  input=True,
+                                  input_device_index=int(id),
+                                  stream_callback=self.callback)
+
+    def callback(self, in_data, frame_count, time_info, flag):
+        # using Numpy to convert to array for processing
+        audio_data = np.fromstring(in_data, dtype=np.float32)
+        self.plot_data(audio_data)
+        return in_data, pyaudio.paContinue
+
+
+if __name__ == "__main__":
+    ads = AudioSampler()
+    ads.start_stream()
