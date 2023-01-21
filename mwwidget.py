019365d00ef88b4919aa3233bdc707fe83a4bd39
Bryce W <58643544+02bwilson@users.noreply.github.com>
2023-01-20 19:31:01 -0500

    Overhaul, added table, audio device selection, audio display

--- mwwidget.py
+++ mwwidget.py
@@ -1,5 +1,10 @@
+import time
+
 from PyQt6 import QtWidgets, QtGui, QtCore
+from PyQt6.QtCore import QThread
 from PyQt6.QtWidgets import QHeaderView
+from audiosampler import AudioSampler
+import pyqtgraph as pg
 
 
 class MainWidget(QtWidgets.QWidget):
@@ -12,14 +17,41 @@ class MainWidget(QtWidgets.QWidget):
         self.decode_modle = None
         self.decode_table = None
         self.form_layout = None
+        self.audio_sampler = None
+        self.audio_devices = None
+        self.audio_select = None
+        self.graphWidget = None
+        self.timer = None
+        self.plot_line = None
 
         # Setup basic stuff
         self.layout = QtWidgets.QVBoxLayout()
         self.form_layout = QtWidgets.QFormLayout()
+        self.audio_select = QtWidgets.QComboBox()
+        self.graphWidget = pg.PlotWidget()
+        self.graphWidget.setMouseEnabled(x=False, y=False)
         self.setup_table()
 
         self.layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+
+        self.audio_sampler = AudioSampler(mw=self)
+        self.audio_sampler.update.connect(self.plot_data)
+        self.audio_devices = self.audio_sampler.get_audio_devices()
+
+        self.audio_select.addItems(self.audio_devices)
+        self.audio_select.currentIndexChanged.connect(self.change_audio_device)
+
+        self.layout.addWidget(self.audio_select)
+        self.layout.addWidget(self.graphWidget)
+        self.plot_line = self.graphWidget.plot([0])
+
         self.setLayout(self.layout)
+
+    def plot_data(self, data):
+        self.plot_line.setData(data)
+
+    def change_audio_device(self):
+        self.audio_sampler.set_audio_device(self.audio_select.currentText().split(" ")[0])
 
     def setup_table(self):
         self.decode_model = QtGui.QStandardItemModel()
